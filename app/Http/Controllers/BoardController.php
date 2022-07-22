c99f347f020755c5d65930a59cdbffb927a247a7
vvuru <wown5398@gmail.com>
2022-07-22 15:11:51 +0900

    show list

---
+++ app/Http/Controllers/BoardController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Board;
+
+class BoardController extends Controller
+{
+    // p.77 라우트명의 관례
+
+    public function index()
+    {
+        $list = Board::select(['id', 'title', 'hits', 'created_at'])->orderBy('id', 'desc')->get();
+        return view('board/index')->with('list', $list);
+        // return view('board/index')->with('list', Board::orderBy('id','desc')->get()); 
+    }
+
+    public function create()
+    {
+        return view('board/create');
+    }
+
+    public function store(Request $req)
+    {
+        $board = new Board([
+            "title" => $req->input("title"),
+            "ctnt" => $req->input("ctnt"),
+            "hits" => 0,
+        ]);
+        $board->save();
+        return redirect('/boards');
+    }
+
+    public function show()
+    {
+        return view('board/show');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BoardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('boards')->group(function () {
+    Route::get('/', [BoardController::class, 'index']);
+    Route::get('create', [BoardController::class, 'create'])->name('boards.create');
+    Route::post('store', [BoardController::class, 'store'])->name('boards.store');
+    Route::get('show', [BoardController::class, 'show'])->name('boards.show');
 });
